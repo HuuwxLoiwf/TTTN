3369dfa98ad14375119facced45d51a9d33ae963
HuuwxLoiwf <huuloi21082004@gmail.com>
2026-05-08 23:59:15 +0700

    L

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -6,17 +6,32 @@ export const inngest = new Inngest({ id: "project-management" });
 
 //Inngest functions save user
 const syncUserCreation = inngest.createFunction(
-    { id: "sync/user-creation", triggers: [{ event: "clerk.user.created" }] },
-    async ({ event }) => {
-        const { data } = event;
-        await prisma.user.create({
-            data: {
-                id: data.id,
-                email: data?.email_addresses[0]?.email_address,
-                name: data?.first_name + " " + data?.last_name,
-                image: data?.image_url,
-            }
-        });
+//     { id: "sync/user-creation", triggers: [{ event: "clerk.user.created" }] },
+//     async ({ event }) => {
+//         const { data } = event;
+//         await prisma.user.create({
+//             data: {
+//                 id: data.id,
+//                 email: data?.email_addresses[0]?.email_address,
+//                 name: data?.first_name + " " + data?.last_name,
+//                 image: data?.image_url,
+//             }
+//         });
+//     }
+// );
+
+{ id: "sync/user-creation", triggers: [{ event: "clerk/user.created" }] },
+async ({ event }) => {
+    const { data } = event;
+
+    await prisma.user.create({
+        data: {
+            id: data.id,
+            email: data?.email_addresses[0]?.email_address,
+            name: data?.first_name + " " + data?.last_name,
+            image: data?.image_url,
+        }
+    });
     }
 );
 
